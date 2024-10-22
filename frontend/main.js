b0a28f613a992228f5e7d8e658cbdff18396622e
Juan Duran <Juan.Duran@coastsign.com>
2024-10-22 08:10:29 -0700

    Made some changes regarding connecting to Azure

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://azureresumefunctionget.azurewebsites.net/api/GetResumeCounter?code=-yGVF8nkJnkDyT6IjtlAYPzn8gDTv7cSJp3iSk8UzLgqAzFu9Gl9Kw%3D%3D'
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30; // Default count value in case of error
-    fetch(functionApi)
+    fetch(functionApiUrl)
         .then(response => response.json())
         .then(data => {
             console.log("Website called function API.");
